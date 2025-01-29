3463835b0eaad311e2d857075fde46b7d1def7fc
Reicancode <izengaliev@gmail.com>
2025-01-29 21:18:42 +0500

    first commit

---
+++ src/main/java/com/github/danat/DanatApplication.java
@@ -0,0 +1,13 @@
+package com.github.danat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/danat/DanatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
